8b5659bdaa269cdac91746b8130f3b40b10db883
bombon90 <rodrigo.medina3774@gmail.com>
2025-02-28 20:20:25 -0600

    Add files via upload

---
+++ errores.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+try:
+    if´carlos´ == carlos,nombre:
+except NameError:
+    print("Define la variable carlos primero")
+
+Escribe tu edad: 22
+24
+Define la variable carlos primero
+
+
+# In[18]:
+
+
+jamaica = input("pon un numero")
+pedro = jamica + 1
+
+
+# In[29]:
+
+
+lista = [1,2,3]
+print(lista[5]
+
+
+# In[31]:
+
+
+a = 10
+b = 0
+c = a / b
+
+
+# In[33]:
+
+
+X = 5 
+if  x < 0:
+    raise valuerror("el valor no nse puedeser negativo")
+
+
+# In[35]:
+
+
+def saludar():
+print(hola")
+
+
+# In[37]:
+
+
+mi_dict =  á´: 1, ´b´: 2}
+print(lista[5])
+
+
+# In[43]:
+
+
+def calcular_area(base, altura):
+    return base * altura  # Corregido
+
+print(cadfrea(5, 10))  
+
+
+# In[45]:
+
+
+lista = [1, 2, 3]
+print(lista[5])  
+
+
+# In[49]:
+
+
+if b != 0:
+    print(a / b)
+else: dividir por cero.")
+
+    print("No se puede
+
+
+# In[51]:
+
+
+a = 5 + "5"  # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
