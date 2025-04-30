841b11ed38279b68a7aaf91d7d68862f9b89e670
bombon90 <rodrigo.medina3774@gmail.com>
2025-04-30 16:13:05 -0600

    Add files via upload

---
+++ sqlite.sql
@@ -0,0 +1,13 @@
+CREATE TABLE MATERIAS1 (ID INT, NOMBRE VARCHAR(30), GRADO INT);
+INSERT INTO MATERIAS1(CODIGO,NOMBRE,ALUMNO,CALIFICACION VALUES
+
+(1, 'gabriel',10);
+(2,'juan',9);
+(3,'uriel',6);
+(4,'cesar',7);
+(5,'aldahir',9);
+(6,'estefania',10);
+(7,'edgar',10);
+(8,'rodrigo',10);
+(9,'tania',8);
+(10,'sergio',7);
